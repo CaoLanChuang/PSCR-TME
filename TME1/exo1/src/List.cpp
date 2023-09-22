657fb733c787699feaf5a8eb7092571d98db1a9e
CaoLanChuang <caolanchuang@gmail.com>
2023-09-22 14:44:23 +0200

    OOK

--- TME1/exo1/src/List.cpp
+++ TME1/exo1/src/List.cpp
@@ -10,15 +10,15 @@ size_t Chainon::length() {
 	if (next != nullptr) {
 		len += next->length();
 	}
-	return length();
+	return len;
 }
 
 void Chainon::print (std::ostream & os) const {
 	os << data ;
 	if (next != nullptr) {
 		os << ", ";
+		next->print(os);
 	}
-	next->print(os);
 }
 
 // ******************  List
