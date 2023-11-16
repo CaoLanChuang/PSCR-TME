ef30e298965f1d47aa582fdb82bd351abdff7a8f
LiXiaoquanSU <xiaoquan.li@etu.sorbonne-universite.fr>
2023-11-16 15:07:23 +0100

    6

--- TME5/src/Queue.h
+++ TME5/src/Queue.h
@@ -7,100 +7,85 @@
 
 namespace pr {
 
-// MT safe version of the Queue, non blocking.
-template <typename T>
-class Queue 
-{
-	T ** tab;
-	const size_t allocsize;
-	size_t begin;
-	size_t sz;
-	mutable std::recursive_mutex m;
-	std::condition_variable_any cv;
-	bool isBlocking = true;
-	
+    template <typename T>
+    class Queue {
+        T ** tab;
+        const size_t allocsize;
+        size_t begin;
+        size_t sz;
+        mutable std::mutex m;
+        std::condition_variable cond_vide;
+        std::condition_variable cond_plein;
+        bool isBlocking;
 
-	// fonctions private, sans protection mutex
-	bool empty() const 
-	{
-		std::unique_lock<std::recursive_mutex> lg(m);
-		return sz == 0;
-	}
+        bool empty() const {
+            return sz == 0;
+        }
+        bool full() const {
+            return sz == allocsize;
+        }
 
-	bool full() const 
-	{
-		std::unique_lock<std::recursive_mutex> lg(m);
-		return sz == allocsize;
-	}
+    public:
 
-public:
+        Queue(size_t size) :allocsize(size), begin(0), sz(0),isBlocking(true) {
+            tab = new T*[size];
+            for(size_t i = 0; i < size; ++i) {
+                tab[i] = nullptr;
+            }
+        }
 
-	Queue(size_t size) :allocsize(size), begin(0), sz(0) 
-	{
-		tab = new T*[size];
-		memset(tab, 0, size * sizeof(T*));
-	}
+        size_t size() const {
+            std::unique_lock<std::mutex> lg(m);
+            return sz;
+        }
 
-	size_t size() const 
-	{
-		std::unique_lock<std::recursive_mutex> lg(m);
-		return sz;
-	}
+        T* pop() {
+            std::unique_lock<std::mutex> lg(m);
+            while (isBlocking && empty()) {
+                cond_vide.wait(lg);
+            }
+            if (empty()) {
+                return nullptr;
+            }
+            auto ret = tab[begin];
+            tab[begin] = nullptr;
+            sz--;
+            begin = (begin + 1) % allocsize;
+            cond_plein.notify_all();
+            return ret;
+        }
 
-	T* pop() 
-	{
-		std::unique_lock<std::recursive_mutex> lg(m);
-		if (empty() && isBlocking) 
-		{
-			cv.wait(lg);
-		}
+        bool push(T* elt) {
+            std::unique_lock<std::mutex> lg(m);
+            while (isBlocking && full()) {
+                cond_plein.wait(lg);
+            }
+            if (full()) {
+                return false;
+            }
+            tab[(begin + sz) % allocsize] = elt;
+            sz++;
+            cond_vide.notify_all();
+            return true;
+        }
 
-		if (!isBlocking && empty())
-		{
-			return nullptr;
-		}
-		
-		auto ret = tab[begin];
-		tab[begin] = nullptr;
-		sz--;
-		begin = (begin + 1) % allocsize;
-		return ret;
-	}
+        void setBlocking(bool block) {
+            std::unique_lock<std::mutex> lg(m);
+            isBlocking = block;
+            if (!isBlocking) {
+                cond_vide.notify_all();
+                cond_plein.notify_all();
+            }
+        }
 
-	bool push(T* elt) 
-	{
-		std::unique_lock<std::recursive_mutex> lg(m);
-
-		while(empty())
-		{
-			cv.wait(lg);
-		}
-
-		
-		/*if (full()) 
-		{
-			return false;
-		}*/
-
-
-
-		tab[(begin + sz) % allocsize] = elt;
-		sz++;
-		if (sz==1) cv.notify_all();
-		return true;
-	}
-
-	~Queue() 
-	{
-		// ?? lock a priori inutile, ne pas detruire si on travaille encore avec
-		for (size_t i = 0; i < sz; i++) 
-		{
-			auto ind = (begin + i) % allocsize;
-			delete tab[ind];
-		}
-		delete[] tab;
-	}
-};
+        ~Queue() {
+            for (size_t i = 0; i < sz; i++) {
+                auto ind = (begin + i) % allocsize;
+                delete tab[ind];
+            }
+            delete[] tab;
+        }
+    };
 
 }
 
