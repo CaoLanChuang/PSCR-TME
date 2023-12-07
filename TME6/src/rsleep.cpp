e40328f6a048153f30e80cb4596fe118415e8a24
CaoLanChuang <caolanchuang@gmail.com>
2023-12-07 17:53:13 +0100

    Finale TME6

--- TME6/src/rsleep.cpp
+++ TME6/src/rsleep.cpp
@@ -4,14 +4,17 @@
 
 void randsleep() 
 {
-  int r = rand();
-  double ratio = (double)r / (double) RAND_MAX;
+  int r = rand();     //生成随机数
+  double ratio = (double)r / (double) RAND_MAX;   //r的值在0到RAND_MAX之间
   struct timespec tosleep;
-  tosleep.tv_sec =0;
+  tosleep.tv_sec =0;    //默认设置为睡眠0秒
+  
   // 300 millions de ns = 0.3 secondes
-  tosleep.tv_nsec = 300000000 + ratio*700000000;
+  tosleep.tv_nsec = 300000000 + ratio*700000000;  //总睡眠时间在0.3秒到1秒之间变化
+
   struct timespec remain;
-  while ( nanosleep(&tosleep, &remain) != 0) 
+
+  while ( nanosleep(&tosleep, &remain) != 0) //如果nanosleep被信号中断，那么会将剩余时间写入remain，然后使用remain继续休眠
   {
     tosleep = remain;
   }
--- TME6/src/wait.cpp
+++ TME6/src/wait.cpp
@@ -1,67 +1,53 @@
-//#include <iostream>
-//#include <signal.h>
-//#include <unistd.h>
-//#include <sys/wait.h>
-//
-//volatile pid_t terminated_pid = -1;
-//
-//void child_terminated(int signo) {
-//    int status;
-//    terminated_pid = wait(&status);
-//}
-//
-//void alarm_triggered(int signo) {
-//    // ne faire rien
-//}
-//
-//int wait_till_pid(pid_t pid) {
-//    pid_t terminated_pid;
-//    int status;
-//    while ((terminated_pid = wait(&status)) != -1) {
-//        if (terminated_pid == pid) {
-//            return pid;
-//        }
-//    }
-//    return -1;
-//}
-//
-//int wait_till_pid(pid_t pid, int sec) {
-//    struct sigaction sa;
-//    sa.sa_handler = child_terminated;
-//    sigemptyset(&sa.sa_mask);
-//    sa.sa_flags = 0;
-//    sigaction(SIGCHLD, &sa, NULL);
-//
-//    sa.sa_handler = alarm_triggered;
-//    sigaction(SIGALRM, &sa, NULL);
-//
-//    alarm(sec);
-//
-//    while (true) {
-//        pause();  // attendre signal
-//        if (terminated_pid == pid) {
-//            alarm(0);  // annuler alarm
-//            return pid;
-//        }
-//        if (terminated_pid != -1) {
-//            return -1;
-//        }
-//    }
-//}
-//
-//int main() {
-//    pid_t pid = fork();
-//    if (pid == 0) { //fils
-//
-//        sleep(5);  // 5s
-//        exit(0);
-//    } else {   //parent
-//
-//        std::cout << "Attente de la fin de processus fils."<<std::endl;
-//        if (wait_till_pid(pid, 10) == pid) {
-//            std::cout << "Le fils processus est terminé."<<std::endl;
-//        }
-//    }
-//    return 0;
-//}
-//
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <csignal>
+
+volatile sig_atomic_t child_terminated = 0;
+volatile sig_atomic_t timeout = 0;
+
+// SIGALRM的处理器
+void alarm_handler(int sig) {
+    timeout = 1;
+}
+
+// SIGCHLD的处理器
+void child_handler(int sig) {
+    child_terminated = 1;
+}
+
+int wait_till_pid(pid_t pid, int sec) {
+    struct sigaction sa;
+
+    // 设置SIGALRM的处理器
+    sa.sa_handler = alarm_handler;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGALRM, &sa, nullptr);
+
+    // 设置SIGCHLD的处理器
+    sa.sa_handler = child_handler;
+    sigaction(SIGCHLD, &sa, nullptr);
+
+    alarm(sec); // sec倒计时结束后触发ALRM信号
+
+    while (true) {
+        if (timeout) {  // 如果闹钟信号被触发了
+            return 0;  // 超时返回0
+        }
+
+        if (child_terminated) 
+        {               // 如果子进程已经结束
+            alarm(0);   // 取消闹钟
+            
+            pid_t p = wait(nullptr); // 清理子进程确保不会变成僵尸进程
+            if (p == -1 || p == pid) 
+            {
+                return p;
+            }
+        }
+    }
+}
