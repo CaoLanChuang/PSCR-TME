aaa329d8478f474df2e9488b0f30564a59483b96
LiXiaoquanSU <xiaoquan.li@etu.sorbonne-universite.fr>
2023-12-08 12:06:42 +0100

    TME5

--- TME5/src/Pool.h
+++ TME5/src/Pool.h
@@ -10,9 +10,10 @@ namespace pr {
 
     class Pool {
     private:
-        Queue<Job> queue;
-        std::vector<std::thread> threads;
+        Queue<Job> queue;//任务队列，用于存储待处理的任务（Job 对象）
+        std::vector<std::thread> threads;//包含所有工作线程的向量
 
+        //这是一个工作线程的函数。它不断地从队列中取出任务（Job 对象），执行它们，然后删除这些对象。
         static void poolWorker(Queue<Job>& queue) {
             while (true) {
                 Job* j = queue.pop();
@@ -25,21 +26,21 @@ namespace pr {
         }
 
     public:
-        Pool(int qsize) : queue(qsize) {}
+        Pool(int qsize) : queue(qsize) {}//构造函数，初始化具有指定大小的任务队列。
 
-        void start(int nbthread) {
+        void start(int nbthread) {//启动线程池，创建指定数量的工作线程。
             for (int i = 0; i < nbthread; ++i) {
                 threads.emplace_back(poolWorker, std::ref(queue));
             }
         }
 
-        void submit(Job* job) {
+        void submit(Job* job) {//向任务队列提交一个新任务。如果队列满了，它将等待直到有空间可用。
             while (!queue.push(job)) {
                 std::this_thread::yield();
             }
         }
 
-        void stop() {
+        void stop() {//停止所有工作线程并清理。它通过向队列中添加空指针作为任务来通知线程结束执行。
             for (int i = 0; i < threads.size(); ++i) {
                 queue.push(nullptr);
             }
@@ -50,7 +51,7 @@ namespace pr {
             }
         }
 
-        ~Pool() {
+        ~Pool() {//析构函数，调用 stop 方法来确保所有线程都被适当地清理。
             stop();
         }
     };
--- TME5/src/barrier.h
+++ TME5/src/barrier.h
@@ -6,23 +6,23 @@
 namespace pr {
 
     class Barrier {
-        mutable std::mutex m;
-        std::condition_variable cond;
-        int cpt = 0;
-        int goal;
+        mutable std::mutex m;//互斥锁
+        std::condition_variable cond;//条件变量
+        int cpt = 0;//计数器，用于跟踪已经到达屏障的线程数量。
+        int goal;//目标值，表示需要达到屏障的线程总数。
 
     public:
-        Barrier(int n) : goal(n) {}
+        Barrier(int n) : goal(n) {}//构造函数，接受一个参数n，表示屏障的目标线程数。
 
-        void done() {
+        void done() {//线程调用此方法以表明它已到达屏障。当所有线程都调用了
             std::unique_lock<std::mutex> lg(m);
             cpt++;
             if (cpt == goal) {
-                cond.notify_all();
+                cond.notify_all();//屏障打开，所有等待的线程可以继续执行。
             }
         }
 
-        void waitFor() {
+        void waitFor() {//线程调用此方法在屏障处等待。如果cpt还没有达到goal，线程将在这里被阻塞。
             std::unique_lock<std::mutex> lg(m);
             cond.wait(lg, [this] { return cpt == goal; });
         }
