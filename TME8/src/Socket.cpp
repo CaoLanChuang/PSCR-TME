4bc7c2dc90d1051f23d34fbebd4ccbcf2714b2b4
LiXiaoquanSU <xiaoquan.li@etu.sorbonne-universite.fr>
2023-12-13 15:09:45 +0100

    tme8

--- TME8/src/Socket.cpp
+++ TME8/src/Socket.cpp
@@ -1,8 +1,10 @@
 #include "Socket.h"
-#include <netdb.h>        // getaddrinfo, freeaddrinfo, getnameinfo
-#include <cstring>        // memset
-#include <unistd.h>        // close
-#include <arpa/inet.h>    // inet_ntoa
+#include <netdb.h>        // getaddrinfo, 将主机名和服务名转换为地址信息
+                        //freeaddrinfo, 释放 getaddrinfo 函数返回的 addrinfo 结构链表所占用的内存
+                        //getnameinfo将地址信息（如IP地址和端口）转换为对应的主机名和服务名
+#include <cstring>        // memset设置内存区域
+#include <unistd.h>        // close关闭一个文件描述符，释放与之关联的资源
+#include <arpa/inet.h>    // inet_ntoa将网络地址转换为点分十进制的字符串格式
 #include <iostream>
 
 
@@ -10,7 +12,7 @@ void pr::Socket::connect(const std::string & host, int port)
 {
     struct addrinfo* ai;
     struct in_addr ipv4;
-    if(getaddrinfo(host.c_str(),NULL,NULL,&ai)!= 0)
+    if(getaddrinfo(host.c_str(),NULL,NULL,&ai)!= 0)//解析提供的主机名，获取地址信息
 	{
         perror("DNS");
         exit(1);
@@ -19,6 +21,7 @@ void pr::Socket::connect(const std::string & host, int port)
     for(struct addrinfo* rp = ai; rp !=NULL; rp = rp->ai_next)
 	{
         if(rp->ai_family == AF_INET  /*IPv4 */ && ai->ai_socktype == SOCK_STREAM /*TCP*/){
+            //查找IPv4地址（AF_INET）和TCP套接字类型（SOCK_STREAM）的条目
             ipv4 = (((struct sockaddr_in*) rp->ai_addr)->sin_addr);
             break;
         } 
@@ -51,6 +54,7 @@ void pr::Socket::connect(in_addr ipv4, int port)
     }
 }
 
+//判断如果套接字打开，则关闭连接和描述符
 void pr::Socket::close()
 {
     if(fd!=-1) {
@@ -59,7 +63,9 @@ void pr::Socket::close()
     }
 }
 
-
+//用于输出 sockaddr_in 结构的内容到输出流。
+//使用 getnameinfo 函数尝试获取主机名。
+//输出IP地址和端口号（端口号使用 ntohs 函数转换为主机字节顺序）。
 std::ostream & operator<< (std::ostream & os, struct sockaddr_in & addr)
 {
     
@@ -71,9 +77,6 @@ std::ostream & operator<< (std::ostream & os, struct sockaddr_in & addr)
         os << "host:" << host << std::endl;
     }
     std::cout << inet_ntoa(addr.sin_addr) << ":" <<  ntohs(addr.sin_port) << std::endl; 
-
-
-
 
     return os;
 }
--- TME8/src/Socket.h
+++ TME8/src/Socket.h
@@ -13,12 +13,12 @@ namespace pr
 		int fd;
 
 		public :
-		Socket():fd(-1){}
-		Socket(int fd):fd(fd){}
+		Socket():fd(-1){}//-1，表示无效的文件描述符
+		Socket(int fd):fd(fd){}//接受一个整数参数用于初始化 fd
 
-		// tente de se connecter à l'hôte fourni
-		void connect(const std::string & host, int port);
-		void connect(in_addr ipv4, int port);
+		// tente de se connecter à l'hôte fourni建立与指定主机的连接
+		void connect(const std::string & host, int port);//接受一个字符串（代表主机名）和端口号
+		void connect(in_addr ipv4, int port);//接受一个 in_addr 结构（代表IPv4地址）和端口号
 
 		bool isOpen() const 
 		{
